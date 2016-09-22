e914a3efb98e53fd98e13e7e35987cd423776e5f
jasvir99 <jassigrewal91@gmail.com>
2016-09-21 19:01:07 -0700

    adds jFrame

--- src/khoj/Khoj.java
+++ src/khoj/Khoj.java
@@ -16,6 +16,12 @@ public class Khoj {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainForm().setVisible(true);
+            }
+        });
     }
     
 }
