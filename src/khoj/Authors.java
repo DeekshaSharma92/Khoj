e837c91c948a43e6a9a150490a57c4f1276d445c
jasvir99 <jassigrewal91@gmail.com>
2016-10-02 14:37:51 -0700

    mapping class for author json files

---
+++ src/khoj/Authors.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package khoj;
+
+import com.google.gson.JsonPrimitive;
+
+/**
+ *
+ * @author jass
+ */
+public class Authors {
+    JsonPrimitive body;
+    JsonPrimitive url;
+    JsonPrimitive title;
+    JsonPrimitive subtitle;
+    JsonPrimitive author;
+}
